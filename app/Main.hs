21dd8ac99762985b9681c697d58226b86e5eed3c
Hans Hoglund <hans.hoglund@sc.com>
2018-03-07 16:20:03 +0000

    Try to get CCC/E to work

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,24 @@
 {-# LANGUAGE TypeOperators, TypeFamilies, DataKinds, GADTs, FlexibleContexts,
+MultiParamTypeClasses,
 
 
 UndecidableInstances #-}
 
-import Prelude hiding (take, drop, (++))
-import Data.Vector.Generic.Sized
+import Prelude hiding ((.), id, take, drop, head, (++))
+import Data.Vector.Sized
 import qualified Data.Vector as V
 import GHC.TypeLits
 import Data.Word
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Proxy
+import Control.Lens
+import CCC (CCC(..))
+import Data.Coerce
+import Control.Category
 -- Goal: provide a ZKP of "(The byte sequence S applied to T yields R) and hashing s yields H" (S being the hidden quantity)
 
-type Vec = Vector V.Vector
+type Vec = Vector
 
 -- | A function, represented as a boolean matrix.
 newtype (~>) a b = F { runF :: (Vec (Card a * Size b) BoolExp) }
@@ -21,21 +26,35 @@ newtype (~>) a b = F { runF :: (Vec (Card a * Size b) BoolExp) }
 instance (HasBoolRep a, HasBoolRep b) => HasBoolRep (a ~> b) where
   type Size (a ~> b) = Card a * Size b
   type Card (a ~> b) = Card b ^ Card a
+  rep = iso coerce coerce
 
 data E :: * -> * -> * where
-  Id    :: (KnownNat (Size a)) => E a a
+  Id    :: E a a
   Comp  :: E b c -> E a b -> E a c
-  Eval  :: E (a, a ~> b) b
+  Eval  :: E (a ~> b, a) b
     -- R ^esult waits for a and a table, then treats as a number in table lookup
   Curry :: E (a, b) c -> E a (b ~> c)
     -- result waits for a, then makes a table with all poss b, using an evaluated form of the given expr
     -- which is (Vec (Size a + Size b) BoolExp -> Vec Size c BoolExp)
   Uncurry :: E a (b ~> c) -> E (a, b) c
     -- Given a and b, runs the given for a and looks up b in table (specialized version of Eval?)
-  Fst   :: (KnownNat (Size a), KnownNat (Size b)) => E (a, b) a
-  Snd   :: (KnownNat (Size a), KnownNat (Size b)) => E (a, b) b
-  Pair  :: (KnownNat (Size a), KnownNat (Size b)) => E a b -> E a c -> E a (b,c)
+  Fst   :: E (a, b) a
+  Snd   :: E (a, b) b
+  Pair  :: E a b -> E a c -> E a (b,c)
   Unit  :: E a ()
+
+instance Category E where
+  id  = Id
+  (.) = Comp
+
+instance CCC E (~>) (,) () where
+  eval = Eval
+  curry = Curry
+  uncurry = Uncurry
+  fork = Pair
+  exl = Fst
+  exr = Snd
+  unit = Unit
 
 -- inSize :: HasBoolRep a => e a b -> Int
 -- inSizeFst :: HasBoolRep a => e (a, x) b -> Int
@@ -47,13 +66,16 @@ class HasBoolRep a where
   type Card a :: Nat --how many elements in the set
 --   type Elem a :: * -- cardinality
 --
+  rep :: Iso' a (Vec (Size a) BoolExp)
 
 instance HasBoolRep () where
   type Size () = 1
   type Card () = 1
+  rep = iso (pure True) (const ())
 instance HasBoolRep Bool where
   type Size Bool = 1
   type Card Bool = 2^1
+  rep = iso pure head
 instance HasBoolRep Word8 where
   type Size Word8 = 8
   type Card Word8 = 2^8
@@ -83,7 +105,8 @@ evalBoolExp env = go
 
 --
 compile ::
-
+  (HasBoolRep a, HasBoolRep b)
+  =>
   E a b -> Vec (Size a) BoolExp -> Vec (Size b) BoolExp
 compile Id = id
 compile (Comp f g) = compile f . compile g
